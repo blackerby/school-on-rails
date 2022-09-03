10558b354d65fb63ca3210de679abea7e37e3aaf
blackerby <wmblackerby@gmail.com>
2022-09-03 12:16:01 -0500

    Build out teacher editing

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,4 +6,15 @@ class TeachersController < ApplicationController
   def show
     @teacher = Teacher.find(params[:id])
   end
+
+  def edit
+    @teacher = Teacher.find(params[:id])
+  end
+
+  def update
+    @teacher = Teacher.find(params[:id])
+    teacher_params = params.require(:teacher).permit(:first_name, :last_name, :email)
+    @teacher.update(teacher_params)
+    redirect_to @teacher
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'teachers#index'
+
   get '/teachers', to: 'teachers#index'
-  get '/teachers/:id', to: 'teachers#show'
+  get '/teachers/:id', to: 'teachers#show', as: 'teacher'
+  get '/teachers/:id/edit', to: 'teachers#edit', as: 'edit_teacher'
+  patch '/teachers/:id', to: 'teachers#update'
 end
