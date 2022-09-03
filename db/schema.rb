a372359b8ec7476504aaf44c7e78b6ab2f933768
blackerby <wmblackerby@gmail.com>
2022-09-03 13:23:38 -0500

    Begin work on department model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_03_031533) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_182011) do
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "teachers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
