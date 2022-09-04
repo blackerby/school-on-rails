767b5ee8c22ee68157bf5c9a32e693309c95c9b5
blackerby <wmblackerby@gmail.com>
2022-09-04 10:08:41 -0500

    Add list partial for index pages

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,8 @@ class Teacher < ApplicationRecord
   belongs_to :department, optional: true
 
   validates :first_name, :last_name, :email, presence: true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
