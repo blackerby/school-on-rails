0034c0826ec78adc7670f6bfdad61ca0a7ed545e
blackerby <wmblackerby@gmail.com>
2022-09-07 15:27:13 -0500

    Set up bootstrap gem and Hotwire status bar

--- Gemfile
+++ Gemfile
@@ -24,6 +24,10 @@ gem 'stimulus-rails'
 
 gem 'sassc-rails'
 
+# Bootstrap
+gem 'jquery-rails'
+gem 'bootstrap', '~> 5.2.0'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
+//= require jquery3
+//= require popper
+//= require bootstrap
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+// Turbo.session.drive = false;
+Turbo.setProgressBarDelay(1);
