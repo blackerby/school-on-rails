7ff5685392ed29b8403171ab35b1a501b050c2a8
blackerby <wmblackerby@gmail.com>
2022-09-07 21:56:48 -0500

    Add before_action to MeetingsController and create Add Class turbo frame

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,34 +1,31 @@
 class MeetingsController < ApplicationController
+  before_action :set_teacher
+
   def new
-    @teacher = Teacher.find(params[:teacher_id])
     @meeting = @teacher.meetings.new
   end
 
   def create
-    @teacher = Teacher.find(params[:teacher_id])
     @meeting = @teacher.meetings.new(meeting_params)
 
     if @meeting.save
-      redirect_to teacher_path(@teacher), notice: 'Class meeting successfully created'
+      redirect_to teacher_url(@teacher), notice: 'Class meeting successfully created'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @teacher = Teacher.find(params[:teacher_id])
     @meeting = Meeting.find(params[:id])
     @meeting.destroy
     redirect_to teacher_url(@teacher), status: :see_other, alert: 'Class meeting successfully deleted.'
   end
 
   def edit
-    @teacher = Teacher.find(params[:teacher_id])
     @meeting = Meeting.find(params[:id])
   end
 
   def update
-    @teacher = Teacher.find(params[:teacher_id])
     @meeting = Meeting.find(params[:id])
     if @meeting.update(meeting_params)
       redirect_to @teacher, notice: 'Class meeting successfully updated.'
@@ -41,5 +38,9 @@ class MeetingsController < ApplicationController
 
   def meeting_params
     params.require(:meeting).permit(:classroom_id, :block_id)
+  end
+
+  def set_teacher
+    @teacher = Teacher.find(params[:teacher_id])
   end
 end
