393de535fa5d0557213b15106933bf9dfd5df079
blackerby <wmblackerby@gmail.com>
2022-09-08 20:25:56 -0500

    Fancy debounced Hotwire search but the turbo frame breaks how the links work

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin '@popperjs/core', to: 'popper.js', preload: true
+pin "debounce" # @1.2.1
