e3f573e08f5149007a0b7da114c8e73961b058fc
blackerby <wmblackerby@gmail.com>
2022-09-20 14:30:18 -0500

    Add index and show actions to buildings resource

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,10 @@
 class BuildingsController < ApplicationController
+  def index
+    @buildings = Building.all
+  end
+
+  def show
+    @building = Building.find(params[:id])
+    @classrooms = Classroom.where(building_id: @building.id)
+  end
 end
--- test/controllers/buildings_controller_test.rb
+++ test/controllers/buildings_controller_test.rb
@@ -1,7 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class BuildingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get building index' do
+    get buildings_url
+    assert_response :success
+  end
+
+  test 'should get building show page' do
+    building = buildings(:one)
+
+    get building_url(building)
+    assert_response :success
+  end
 end
