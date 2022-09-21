9e6f26d569a617b2e6bcbdc6daff9c2e15e0f22b
blackerby <wmblackerby@gmail.com>
2022-09-20 21:19:04 -0500

    Fix errors controller tests

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,13 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get not_found" do
+  test 'should get not_found' do
     get errors_not_found_url
-    assert_response :success
+    assert_response 404
   end
 
-  test "should get internal_server_error" do
+  test 'should get internal_server_error' do
     get errors_internal_server_error_url
-    assert_response :success
+    assert_response 500
   end
 end
